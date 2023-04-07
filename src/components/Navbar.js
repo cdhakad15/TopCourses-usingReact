cd6d4b6a66da25855afcf3768fda0d425c3a6abb
CHETAN DHAKAD <120743252+cdhakad15@users.noreply.github.com>
2023-04-07 14:48:02 +0530

    Add files via upload

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <nav className='bg-bgDark py-4'>
+            <h1 className=' text-center font-semibold text-3xl 
+               text-white'>Top Courses</h1>
+        </nav>
+    </div>
+  )
+}
+
+export default Navbar;
